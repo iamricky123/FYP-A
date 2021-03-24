8a6a627e363a05b0e2796238e9d4df0e4cf87f16
Ric <55644095+iamricky123@users.noreply.github.com>
2021-03-24 22:01:09 +0800

    Website
    Website

--- FYP website ver 2/config/pages/urls.py
+++ FYP website ver 2/config/pages/urls.py
@@ -1,9 +1,11 @@
 # pages/urls.py
 from django.urls import path
 
-from .views import HomePageView,AboutUsView
+from .views import HomePageView,AboutUsView,ContactUsView, RegisterView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('aboutus/', AboutUsView.as_view() , name='aboutus'),
+    path('contactus/', ContactUsView.as_view() , name='contactus'),
+    path('register/', RegisterView.as_view() , name='register'),
 ]
--- FYP website ver 2/config/pages/views.py
+++ FYP website ver 2/config/pages/views.py
@@ -1,8 +1,21 @@
 # pages/views.py
 from django.views.generic import TemplateView
+from accounts .models import CustomUser
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = CustomUser.objects.all()
+        return context
+
 
 class AboutUsView (TemplateView):
     template_name = 'aboutus.html'
+
+class ContactUsView (TemplateView):
+    template_name = 'contactus.html'
+
+class RegisterView (TemplateView):
+    template_name = 'registration.html'
