272750dc3fb176397365edc68d114b6a31fd55b0
JLMobile <62094040+JLMobile@users.noreply.github.com>
2021-03-27 14:24:54 +0800

    Update urls.py

--- FYP website ver 2/config/config/urls.py
+++ FYP website ver 2/config/config/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django_otp.admin import OTPAdminSite
+admin.site.__class__=OTPAdminSite
+
+class OTPAdmin(OTPAdminSite):
+    pass
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('accounts/', include('allauth.urls')), # new
