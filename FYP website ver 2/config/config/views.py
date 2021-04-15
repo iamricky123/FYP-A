aca7a3230223df403fb861806b09b242aecf6d3d
Timothy <timchew2008@gmail.com>
2021-04-15 21:14:13 +0800

    Ming

--- FYP website ver 2/config/config/views.py
+++ FYP website ver 2/config/config/views.py
@@ -19,35 +19,27 @@ solreport = (os.path.dirname(__file__) + "\solution.xml")
 def portscanscript(request):
     portscanweb = request.POST.get('param')
     portscandate = date.today()
+    success = False
     try:
         target = socket.gethostbyname(portscanweb)
-        print(target)
         nm = nmap.PortScanner()
         nm.scan(target,'1-500')
+        for host in nm.all_hosts():
+            for proto in nm[host].all_protocols(): #get all scanned protocols ['tcp', 'udp'] in (ip|tcp|udp|sctp)
+                lport = nm[host][proto].keys() #get all ports for tcp/udp protocol
+                for port in sorted(lport):
+                    saveportrecord = UserPortReport()
+                    saveportrecord.date = portscandate
+                    saveportrecord.host_data = target + "[" + portscanweb + "]"
+                    saveportrecord.state = nm[host].state()
+                    saveportrecord.protocol = port
+                    saveportrecord.email = request.user
+                    saveportrecord.save() 
     except:
-        print("Error has occured")
-
-
-    for host in nm.all_hosts():
-        print("Host : %s (%s)" % (host,target))
-        print("State : %s" % nm[host].state()) #get state of host(up|down|unknown|skipped)
-        # now write the loop for finding the protocol
-        for proto in nm[host].all_protocols(): #get all scanned protocols ['tcp', 'udp'] in (ip|tcp|udp|sctp)
-            print("----------" * 6)
-            print("Protocol : %s" % proto)
-            lport = nm[host][proto].keys() #get all ports for tcp/udp protocol
-            for port in sorted(lport):
-                print ("port : %s\tstate : %s" % (port,nm[host][proto][port]['state']))
-
-                saveportrecord = UserPortReport()
-                saveportrecord.date = portscandate
-                saveportrecord.host_data = target
-                saveportrecord.state = nm[host].state()
-                saveportrecord.protocol = port
-                saveportrecord.email = request.user
-                saveportrecord.save()
-
-    return redirect('portscan_redirect/')
+        messages.error(request, 'Could not connect to host, try again')
+        return render(request, 'home.html')
+    
+    return redirect('portscan_redirect/')  
 
 
 
